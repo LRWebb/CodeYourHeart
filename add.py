4db212aa49ce075d9d4ee1a329141702896536d7
coffeefibercable <7729442+LRWebb@users.noreply.github.com>
2018-05-16 17:46:28 -0400

    Add first file

---
+++ add.py
@@ -0,0 +1,7 @@
+"""Take inputs from the user and add them together."""
+number1 = float(input("What is the first number you would like to add? "))
+number2 = float(input("What is the second number you would like to add? "))
+
+sum = (number1) + (number2)
+
+print(f"The sum is {sum}")
